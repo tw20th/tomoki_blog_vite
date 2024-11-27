96628b4c9cd3397d11ce42cab54050fd63439abb
tw20th <51616143+tw20th@users.noreply.github.com>
2024-11-27 15:25:03 +0900

    Prepare Vite project for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,4 @@ import preact from '@preact/preset-vite'
 
 export default defineConfig({
   plugins: [preact()],
-  base: '/tomoki_blog_vite/', // リポジトリ名を指定
-  build: {
-    outDir: 'docs', // GitHub Pages用の出力フォルダ
-  },
 })
